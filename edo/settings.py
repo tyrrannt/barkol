2be7fa92b45dd3554da56aa196c49dca41e543ea
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-01-06 20:57:57 +0300

    deploy2

--- edo/settings.py
+++ edo/settings.py
@@ -15,16 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j1&$q$o(so#g%cxui*r-x*cefo4234f$fm&79-=q8cj2nzu^)%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -71,15 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'edo.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -116,9 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
 
 # Media files (Users images)
 MEDIA_URL = '/media/'
@@ -127,3 +106,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Users auth
 AUTH_USER_MODEL = 'authapp.Person'
 LOGIN_URL = '/authapp/login'
+
+try:
+    from edo.settings_local import *
+except ImportError:
+    from edo.settings_deploy import *
---
+++ edo/settings_deploy.py
@@ -0,0 +1,33 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'wq%Cr|2NKJ4wijqyJ7LnIQriso9xvCwu1RM3Z~IAPAiy8AnzQX'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+}
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
